0d3ba464e4438b0c5430a3c8e0a9908c58e60fbb
DCORQUIR <ingdiego.corts65@gmail.com>
2021-06-09 13:32:03 -0500

    Added adjust for resolve comments in pr

--- src/interfaces/configuration_interface.py
+++ src/interfaces/configuration_interface.py
@@ -14,4 +14,38 @@ class ConfigurationInterface:
         Return:
             Configurations object list
         """
-        return Configuration.objects().objects().all()
+        return Configuration.objects().all()
+
+    @staticmethod
+    def find_by_identifier(identifier: str):
+        """
+        Get a configuration by name
+
+        Parameter:
+            identifier (str): Identifier to search the configuration
+
+        Return:
+            A configuration object
+
+        """
+        filters = dict(
+            identifier=identifier
+        )
+        return Configuration.objects(**filters).first()
+
+    @staticmethod
+    def find_by_name(name: str):
+        """
+        Get a configuration by name
+
+        Parameter:
+            name (str): Name to search the configuration
+
+        Return:
+            A configuration object
+
+        """
+        filters = dict(
+            name=name
+        )
+        return Configuration.objects(**filters).first()
