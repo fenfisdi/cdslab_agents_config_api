dffa07c09d54d2de399d10277d72fbb0bd60410d
miguelgomezg <miguelgomezgarcia@yahoo.com>
2021-06-11 15:45:09 -0500

    reverse in master db

--- master_db.py
+++ master_db.py
@@ -5,282 +5,57 @@ from uuid import uuid1
 from src.models.db.master.distribution import MasterDistribution
 from src.models.db.master.disease_states import MasterDiseaseStates
 
-
-def raise_exception(message: str, err: str):
-    raise KeyError(f"{message}. Error type: {err}")
-
-
 if __name__ == "__main__":
 
-    mongo_uri = environ.get("MONGO_URI")
+    mongo_uri = environ.get('MONGO_URI')
     connect(host=mongo_uri)
 
-    disease_status = [
-        "diagnosis",
-        "quarantine_postdiagnosis",
-        "hospitalization_prob",
-        "ICU_prob"
-    ]
+    disease_status = ["diagnosis", "quarantine_postdiagnosis", "hospitalization_prob", "ICU_prob"]
 
-    type_float_list = [
-        "float",
-        "List[float]"
-    ]
-
-    type_int_list = [
-        "int",
-        "List[int]"
-    ]
-
-    numpy_normal_field_loc = {
-        "name": "loc",
-        "type": type_float_list
-    }
-
-    numpy_normal_field_scale = {
-        "name": "scale",
-        "type": type_float_list
-    }
-
-    numpy_log_normal_field_mean = {
-        "name": "mean",
-        "type": type_float_list
-    }
-
-    numpy_log_normal_field_sigma = {
-        "name": "sigma",
-        "type": type_float_list
-    }
-
-    numpy_gamma_field_shape = {
-        "name": "shape",
-        "type": type_float_list
-    }
-
-    numpy_gamma_field_scale = {
-        "name": "scale",
-        "type": type_float_list
-    }
-
-    numpy_logistic_field_loc = {
-        "name": "loc",
-        "type": type_float_list
-    }
-
-    numpy_logistic_field_scale = {
-        "name": "scale",
-        "type": type_float_list
-    }
-
-    numpy_hyper_geometric_field_n_good_int = {
-        "name": "ngoodint",
-        "type": type_float_list
-    }
-
-    numpy_hyper_geometric_field_n_bad = {
-        "name": "nbad",
-        "type": type_int_list
-    }
-
-    numpy_hyper_geometric_field_n_sample = {
-        "name": "nsample",
-        "type": type_int_list
-    }
-
-    distributions = {
-        "Empirical": [
-            {
-                "Parameter": "bandwidth",
-                "Type": "float",
-                "Field": [1.0]},
-            {
-                "Parameter": "algorithm",
-                "Type": None,
-                "Field": [
-                    "auto",
-                    "kd_tree",
-                    "ball_tree"
-                ]
-            },
-            {
-                "Parameter": "kernel",
-                "Type": None,
-                "Field": [
-                    "gaussian",
-                    "tophat",
-                    "epanechnikov",
-                    "exponential",
-                    "linear",
-                    "cosine"
-                ]
-            },
-            {
-                "Parameter": "metric",
-                "Type": "str",
-                "Field": ["euclidean"]
-            },
-            {
-                "Parameter": "atol",
-                "Type": "float",
-                "Field": [0]
-            },
-            {
-                "Parameter": "rtol",
-                "Type": "float",
-                "Field": [0]
-            },
-            {
-                "Parameter": "breadth_first",
-                "Type": "boolean",
-                "Field": ["True", "False"]
-            },
-            {
-                "Parameter": "leaf_size",
-                "Type": "int",
-                "Field": [40]
-            },
-            {
-                "Parameter": "metric_params",
-                "Type": "dict",
-                "Field": [None]
-            }
-        ],
-        "Constant": [
-            {
-                "Parameter": "Type constant",
-                "Type": "int",
-                "Field": [None]
-            }
-        ],
-        "Weigths": [
-            {
-                "Parameter": "bandwidth",
-                "Type": "float",
-                "Field": [1.0]
-            },
-            {
-                "Parameter": "algorithm",
-                "Type": None,
-                "Field": [
-                    "auto",
-                    "kd_tree",
-                    "ball_tree"
-                ]
-            },
-            {
-                "Parameter": "kernel",
-                "Type": None,
-                "Field": [
-                    "gaussian",
-                    "tophat",
-                    "epanechnikov",
-                    "exponential",
-                    "linear",
-                    "cosine"
-                ]
-            },
-            {
-                "Parameter": "metric",
-                "Type": "str",
-                "Field": ["euclidean"]
-            },
-            {
-                "Parameter": "atol",
-                "Type": "float",
-                "Field": [0]
-            },
-            {
-                "Parameter": "rtol",
-                "Type": "float",
-                "Field": [0]
-            },
-            {
-                "Parameter": "breadth_first",
-                "Type": "boolean",
-                "Field": ["True", "False"]
-            },
-            {
-                "Parameter": "leaf_size",
-                "Type": "int",
-                "Field": [40]
-            }
-        ],
-        "Numpy": [
-            {
-                "Parameter": "normal",
-                "Type": "numpy",
-                "Field": {
-                    numpy_normal_field_loc,
-                    numpy_normal_field_scale
+    distributions = { 
+                "Empirical": [{"Parameter": "bandwidth", "Type": "float", "Field": [1.0]},
+                          {"Parameter": "algorithm", "Type": None, "Field": ["auto", "kd_tree", "ball_tree"]},
+                          {"Parameter": "kernel", "Type": None, "Field": ["gaussian", "tophat", "epanechnikov", "exponential", "linear", "cosine"]},
+                          {"Parameter": "metric", "Type": "str", "Field": ["euclidean"]},
+                          {"Parameter": "atol", "Type": "float", "Field": [0]},
+                          {"Parameter": "rtol", "Type": "float", "Field": [0]},
+                          {"Parameter": "breadth_first", "Type": "boolean", "Field": ["True", "False"]},
+                          {"Parameter": "leaf_size", "Type": "int", "Field": [40]},
+                          {"Parameter": "metric_params", "Type": "dict", "Field": [None]} ],
+                "Constant": [{"Parameter": "Type constant", "Type": "int", "Field": [None]}],
+                "Weigths": [{"Parameter": "bandwidth", "Type": "float", "Field": [1.0]},
+                          {"Parameter": "algorithm", "Type": None, "Field": ["auto", "kd_tree", "ball_tree"]},
+                          {"Parameter": "kernel", "Type": None, "Field": ["gaussian", "tophat", "epanechnikov", "exponential", "linear", "cosine"]},
+                          {"Parameter": "metric", "Type": "str", "Field": ["euclidean"]},
+                          {"Parameter": "atol", "Type": "float", "Field": [0]},
+                          {"Parameter": "rtol", "Type": "float", "Field": [0]},
+                          {"Parameter": "breadth_first", "Type": "boolean", "Field": ["True", "False"]},
+                          {"Parameter": "leaf_size", "Type": "int", "Field": [40]}],
+                "Numpy": [{"Parameter": "normal", "Type": "numpy", "Field": {
+                                        "name": "loc", "type":["float", "List[float]"], 
+                                        "name": "scale", "type":["float", "List[float]"]}},
+                          {"Parameter": "lognormal", "Type": "numpy", "Field": {
+                                        "name": "mean", "type":["float", "List[float]"],
+                                        "name": "sigma", "type":["float", "List[float]"],}},
+                          {"Parameter": "weibull", "Type": "numpy", "Field": {
+                                        "name": "a", "type":["float", "List[float]"]}},
+                          {"Parameter": "gamma", "Type": "numpy", "Field": {
+                                        "name": "shape", "type":["float", "List[float]"],
+                                        "name": "scale", "type":["float", "List[float]"]}},
+                          {"Parameter": "logistic", "Type": "numpy", "Field": {
+                                        "name": "loc", "type":["float", "List[float]"], 
+                                        "name": "scale", "type":["float", "List[float]"]}},
+                          {"Parameter": "poisson", "Type": "numpy", "Field": {
+                                        "name": "Iam", "type":["float", "List[float]"]}},
+                          {"Parameter": "logseries", "Type": "numpy", "Field": {
+                                        "name": "P", "type":["float", "List[float]"]}},
+                          {"Parameter": "geometric", "Type": "numpy", "Field": {
+                                        "name": "P", "type":["float", "List[float]"]}},
+                          {"Parameter": "hypergeometric", "Type": "numpy", "Field": {
+                                        "name": "ngoodint", "type":["int", "List[int]"],
+                                        "name": "nbad", "type":["int", "List[int]"],
+                                        "name": "nsample", "type":["int", "List[int]"]}}]  
                 }
-            },
-            {
-                "Parameter": "lognormal",
-                "Type": "numpy",
-                "Field": {
-                    numpy_log_normal_field_mean,
-                    numpy_log_normal_field_sigma
-                }
-            },
-            {
-                "Parameter": "weibull",
-                "Type": "numpy",
-                "Field": {
-                    "name": "a",
-                    "type": type_float_list
-                }
-            },
-            {
-                "Parameter": "gamma",
-                "Type": "numpy",
-                "Field": {
-                    numpy_gamma_field_shape,
-                    numpy_gamma_field_scale
-                }
-            },
-            {
-                "Parameter": "logistic",
-                "Type": "numpy",
-                "Field": {
-                    numpy_logistic_field_loc,
-                    numpy_logistic_field_scale
-                }
-            },
-            {
-                "Parameter": "poisson",
-                "Type": "numpy",
-                "Field": {
-                    "name": "Iam",
-                    "type": type_float_list
-                }
-            },
-            {
-                "Parameter": "logseries",
-                "Type": "numpy",
-                "Field": {
-                    "name": "P",
-                    "type": type_float_list
-                }
-            },
-            {
-                "Parameter": "geometric",
-                "Type": "numpy",
-                "Field": {
-                    "name": "P",
-                    "type": type_float_list
-                }
-            },
-            {
-                "Parameter": "hypergeometric",
-                "Type": "numpy",
-                "Field": {
-                    numpy_hyper_geometric_field_n_good_int,
-                    numpy_hyper_geometric_field_n_bad,
-                    numpy_hyper_geometric_field_n_sample
-                }
-            }
-        ]
-    }
 
     for name, parameters in distributions.items():
         try:
@@ -289,16 +64,14 @@ if __name__ == "__main__":
                 type=parameters).save()
 
         except Exception as error:
-            msg = f"An error has been occurred while MasterDistribution saved"
-            raise_exception(msg, error)
+            raise RuntimeError(f"No fue posible insertar la distribución {name}, {error}") from error
 
     for name in disease_status:
         try:
             MasterDiseaseStates(
-                identifer=uuid1().hex,
+                identifer=uuid1().hex, 
                 name=name
             ).save()
-
+            
         except Exception as error:
-            msg = f"An error has been occurred while MasterDiseaseStates saved"
-            raise_exception(msg, error)
+            raise RuntimeError(f"No fue posible insertar el estado {name}, {error}") from error
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,13 +1,11 @@
 from .db.configuration import Configuration
 from .db.disease_states import DiseaseStates
 from .db.distribution import Distribution
-from .db.simulation import Simulation
 from .route_models.configuration import NewConfiguration
 
 __all__ = [
     "Configuration", 
     "DiseaseStates", 
     "Distribution", 
-    "Simulation",
     "NewConfiguration"
 ]
