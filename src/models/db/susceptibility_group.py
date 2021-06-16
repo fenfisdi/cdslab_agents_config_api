f69e83d05e7dccf922db29ea91853c1b795a2c55
DCORQUIR <ingdiego.corts65@gmail.com>
2021-06-16 09:44:53 -0500

    Initial model structure for suceptibility group

---
+++ src/models/db/susceptibility_group.py
@@ -0,0 +1,6 @@
+from mongoengine import UUIDField, \
+    ReferenceField, StringField
+
+from .base import BaseDocument
+from configuration import Configuration
+from distribution import Distribution
