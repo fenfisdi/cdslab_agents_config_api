998c3f5c00e61f3ba9a4c82a9f1af5189e84c7f9
DCORQUIR <ingdiego.corts65@gmail.com>
2021-06-16 08:43:39 -0500

    Added unit test for quarantine groups endpoint

--- src/interfaces/tests/test_quarantine_groups.py
+++ src/interfaces/tests/test_quarantine_groups.py
@@ -2,7 +2,6 @@ import uuid
 
 from mongoengine import connect, disconnect
 from unittest import TestCase
-ucm
 from src.models import QuarantineGroup
 from src.interfaces import QuarantineGroupInterface
 
@@ -15,7 +14,35 @@ class QuarantineGroupInterfaceTestCase(TestCase):
             alias="QuarantineGroupInterfaceTestCase"
         )
         self.identifier = uuid.uuid1().hex
+        self.configuration_identifier = uuid.uuid1().hex
         self.name = "Quarantine Group Unit Test"
         self.quarantine_group = QuarantineGroup(
+            identifier=self.identifier,
+            configuration=self.configuration_identifier,
+            name=self.name
+        ).save()
 
+    def tearDown(self):
+        disconnect()
+
+    def test_find_all(self):
+        quarantine_groups = \
+            QuarantineGroupInterface.find_all()
+
+        self.assertIsNotNone(quarantine_groups)
+
+    def test_find_one(self):
+        quarantine_groups = \
+            QuarantineGroupInterface.find_one(self.identifier)
+
+        self.assertIsNotNone(quarantine_groups)
+        self.assertEqual(
+            quarantine_groups.identifier,
+            self.identifier
         )
+
+    def test_find_by_configuration(self):
+        age_groups = QuarantineGroupInterface. \
+            find_by_configuration(self.configuration_identifier)
+
+        self.assertIsNotNone(age_groups)
