2581aaa3290a5fefaa65ce12f2ea2d9cbcf1be55
David Peña <dpenah@intergrupo.com>
2021-06-21 10:58:58 -0500

    Fix: Update Age Group Model

--- src/models/db/age_group.py
+++ src/models/db/age_group.py
@@ -1,24 +1,12 @@
 from mongoengine import (
-    Document,
-    UUIDField,
-    ReferenceField,
-    StringField,
-    FloatField
+    Document, FloatField, ReferenceField, StringField, UUIDField
 )
 
 from .configuration import Configuration
 
 
 class AgeGroup(Document):
-    identifier = UUIDField(
-        binary=False,
-        unique=True,
-        required=True
-    )
-    configuration = ReferenceField(
-        Configuration,
-        dbref=True,
-        required=True
-    )
+    identifier = UUIDField(binary=False, unique=True, required=True)
+    configuration = ReferenceField(Configuration, dbref=True, required=True)
     name = StringField(required=True)
     population_percentage = FloatField(required=True)
