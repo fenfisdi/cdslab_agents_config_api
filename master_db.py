6bbda157e40aba387f6c1c510f0ef9dc209c202b
miguelgomezg <63411891+miguelgomezg@users.noreply.github.com>
2021-06-25 15:11:55 -0500

    Update master_db.py

--- master_db.py
+++ master_db.py
@@ -32,30 +32,39 @@ if __name__ == "__main__":
                           {"Parameter": "rtol", "Type": "float", "Field": [0]},
                           {"Parameter": "breadth_first", "Type": "boolean", "Field": ["True", "False"]},
                           {"Parameter": "leaf_size", "Type": "int", "Field": [40]}],
-                "Numpy": [{"Parameter": "normal", "Type": "numpy", "Field": {
-                                        "name": "loc", "type":["float", "List[float]"], 
-                                        "name": "scale", "type":["float", "List[float]"]}},
-                          {"Parameter": "lognormal", "Type": "numpy", "Field": {
-                                        "name": "mean", "type":["float", "List[float]"],
-                                        "name": "sigma", "type":["float", "List[float]"],}},
-                          {"Parameter": "weibull", "Type": "numpy", "Field": {
-                                        "name": "a", "type":["float", "List[float]"]}},
-                          {"Parameter": "gamma", "Type": "numpy", "Field": {
-                                        "name": "shape", "type":["float", "List[float]"],
-                                        "name": "scale", "type":["float", "List[float]"]}},
-                          {"Parameter": "logistic", "Type": "numpy", "Field": {
-                                        "name": "loc", "type":["float", "List[float]"], 
-                                        "name": "scale", "type":["float", "List[float]"]}},
-                          {"Parameter": "poisson", "Type": "numpy", "Field": {
-                                        "name": "Iam", "type":["float", "List[float]"]}},
-                          {"Parameter": "logseries", "Type": "numpy", "Field": {
-                                        "name": "P", "type":["float", "List[float]"]}},
-                          {"Parameter": "geometric", "Type": "numpy", "Field": {
-                                        "name": "P", "type":["float", "List[float]"]}},
-                          {"Parameter": "hypergeometric", "Type": "numpy", "Field": {
-                                        "name": "ngoodint", "type":["int", "List[int]"],
-                                        "name": "nbad", "type":["int", "List[int]"],
-                                        "name": "nsample", "type":["int", "List[int]"]}}]  
+                "Numpy": [{"Parameter": "normal", "Type": "numpy", "Field": [
+                                        {"name": "loc", "type":["float", "List[float]"]}, 
+                                        {"name": "scale", "type":["float", "List[float]"]}
+                                        ]},
+                          {"Parameter": "lognormal", "Type": "numpy", "Field": [
+                              {"name": "mean", "type":["float", "List[float]"]},
+                              {"name": "sigma", "type":["float", "List[float]"]}
+                              ]},
+                          {"Parameter": "weibull", "Type": "numpy", "Field": [
+                              {"name": "a", "type":["float", "List[float]"]}
+                              ]},
+                          {"Parameter": "gamma", "Type": "numpy", "Field": [
+                                {"name": "shape", "type":["float", "List[float]"]},
+                                {"name": "scale", "type":["float", "List[float]"]}
+                                    ]},
+                          {"Parameter": "logistic", "Type": "numpy", "Field": [
+                              {"name": "loc", "type":["float", "List[float]"]}, 
+                              {"name": "scale", "type":["float", "List[float]"]}
+                              ]},
+                          {"Parameter": "poisson", "Type": "numpy", "Field": [
+                              {"name": "Iam", "type":["float", "List[float]"]}
+                            ]},
+                          {"Parameter": "logseries", "Type": "numpy", "Field": [
+                              {"name": "P", "type":["float", "List[float]"]}
+                            ]},
+                          {"Parameter": "geometric", "Type": "numpy", "Field": [
+                              {"name": "P", "type":["float", "List[float]"]}
+                            ]},
+                          {"Parameter": "hypergeometric", "Type": "numpy", "Field": [
+                              {"name": "ngoodint", "type":["int", "List[int]"]},
+                              {"name": "nbad", "type":["int", "List[int]"]},
+                             {"name": "nsample", "type":["int", "List[int]"]}
+                        ]}]  
                 }
 
     quarantine_restrictions = [
