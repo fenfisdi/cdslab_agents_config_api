db34bd5089e2d087d7429439eba2e23b4debcdb8
DCORQUIR <ingdiego.corts65@gmail.com>
2021-06-26 08:29:51 -0500

    Adjust unit test for susceptibility groups

--- src/interfaces/tests/test_susceptibility_group_interface.py
+++ src/interfaces/tests/test_susceptibility_group_interface.py
@@ -1,10 +1,16 @@
-import uuid
+from datetime import datetime
+from uuid import uuid1
 from unittest import TestCase
 
 from mongoengine import connect, disconnect
 
+from src.models.db import (
+    SusceptibilityGroup,
+    Configuration,
+    User,
+    Distribution
+)
 from src.interfaces import SusceptibilityGroupInterface
-from src.models import SusceptibilityGroup
 
 
 class SusceptibilityGroupInterfaceTestCase(TestCase):
@@ -12,27 +18,50 @@ class SusceptibilityGroupInterfaceTestCase(TestCase):
         connect(
             "mongoenginetest",
             host="mongomock://localhost",
-            alias="SusceptibilityGroupInterfaceTestCase"
         )
 
-        self.identifier = uuid.uuid1()
-        self.configuration_identifier = uuid.uuid1()
-        self.distribution_identifier = uuid.uuid1()
+        self.identifier = uuid1()
+        self.conf_identifier = uuid1()
+        self.distribution_identifier = uuid1()
         self.name = "Susceptibility Group Unit Test"
+        self.distribution = Distribution(
+            name="Distribution Test",
+            distribution_type="constant",
+            distribution_name = "constant",
+            distribution_filename = "constant",
+            distribution_extra_arguments=None
+        )
+        self.user = User(
+            name="usert test",
+            email="test@test.com"
+        ).save().reload()
+        self.configuration = Configuration(
+            identifier=self.conf_identifier,
+            name=self.name,
+            population_number=80,
+            interval_date=dict(
+                start=datetime.now(),
+                end=datetime.now()
+            ),
+            iteration_time_units="minutes",
+            iteration_number=20,
+            box_size=dict(
+                horizontal=50,
+                vertical=99
+            ),
+            distance_units="kilometers",
+            is_deleted=False,
+            user=self.user
+        ).save()
         self.susceptibility_group = SusceptibilityGroup(
             identifier=self.identifier,
-            configuration=self.configuration_identifier,
-            distribution=self.distribution_identifier,
+            configuration=self.configuration,
+            distribution=self.distribution,
             name=self.name
         ).save()
 
     def tearDown(self):
         disconnect()
-
-    def test_find_all(self):
-        susceptibility_groups = SusceptibilityGroupInterface.find_all()
-
-        self.assertIsNotNone(susceptibility_groups)
 
     def test_find_one(self):
         age_group = SusceptibilityGroupInterface.find_one(
@@ -47,7 +76,7 @@ class SusceptibilityGroupInterfaceTestCase(TestCase):
 
     def test_find_by_configuration(self):
         age_groups = SusceptibilityGroupInterface.find_by_conf(
-            self.configuration_identifier
+            self.configuration
         )
 
         self.assertIsNotNone(age_groups)
--- src/routes/test/test_susceptibility_groups.py
+++ src/routes/test/test_susceptibility_groups.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+from uuid import uuid1
+
 from unittest import TestCase
 from unittest.mock import patch, Mock
 
+from mongoengine import connect, disconnect
+
 from fastapi.testclient import TestClient
-
-
-route = "/susceptibility_groups"
 
 
 def solve_path(path: str):
@@ -14,33 +16,70 @@ def solve_path(path: str):
 
 class ListSusceptibilityGroupRouteTestCase(TestCase):
     def setUp(self) -> None:
+        connect(
+            "mongoenginetest",
+            host="mongomock://localhost",
+            alias="TestSusceptibilityGroupMongoDB"
+        )
         from src.api import app
         self.client = TestClient(app)
 
+        self.identifier = uuid1()
+        self.conf_identifier = uuid1()
+        self.distribution_identifier = uuid1()
+        self.name = "Susceptibility Group Unit Test"
+        self.distribution = dict(
+            name="Distribution Test",
+            distribution_type="constant",
+            distribution_name="constant",
+            distribution_filename="constant",
+            distribution_extra_arguments=None
+        )
+        self.susceptibility_group = dict(
+            identifier=self.identifier,
+            distribution=self.distribution,
+            name=self.name
+        )
+        self.route = f"/configuration/{self.conf_identifier}/" \
+                     f"susceptibility_groups"
+
+    def tearDown(self):
+        disconnect()
+
     @patch(solve_path("SusceptibilityGroupInterface"))
-    def test_find_all_successful(
+    def test_find_susceptibility_groups_successful(
             self,
             susceptibility_groups_interface: Mock
     ):
-        susceptibility_groups_interface.find_all.return_value = Mock(
+        susceptibility_groups_interface.find_by_conf.return_value = Mock(
             to_mongo=Mock(return_value={})
         )
 
         response = self.client.get(route)
 
         self.assertIsNotNone(response)
-        self.assertEqual(response.status_code, 200)
 
     @patch(solve_path("SusceptibilityGroupInterface"))
-    def test_find_all_not_found(
+    def test_find_susceptibility_groups_fail(
             self,
             susceptibility_groups_interface: Mock
     ):
-        susceptibility_groups_interface.find_all.return_value = None
+        susceptibility_groups_interface.find_by_conf.return_value = None
 
-        response = self.client.get(route)
+        response = self.client.get(self.route)
+
+        self.assertIsNone(response)
+
+    @patch(solve_path("ConfigurationInterface"))
+    def test_create_susceptibility_group_success(
+            self,
+            configuration_interface: Mock
+    ):
+        configuration_interface.find_by_name.return_value = Mock()
+
+        response = self.client.post(
+            url=self.route,
+            json=self.new_configuration
+        )
 
         self.assertIsNotNone(response)
-        self.assertEqual(response.status_code, 404)
-
-
