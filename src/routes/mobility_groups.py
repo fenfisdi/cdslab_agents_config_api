c9c5488694e16e608f883a15e944bcd52a9b3573
David Peña <dpenah@intergrupo.com>
2021-07-07 11:33:46 -0500

    Fix: Update Mobility Group Endpoints

--- src/routes/mobility_groups.py
+++ src/routes/mobility_groups.py
@@ -1,7 +1,6 @@
-from typing import List
 from uuid import UUID, uuid1
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from starlette.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
@@ -26,7 +25,7 @@ mobility_group_routes = APIRouter(
 )
 
 
-@mobility_group_routes.get("/mobility_groups")
+@mobility_group_routes.get("/mobility_group")
 def list_mobility_groups(
     conf_uuid: UUID,
     user = Depends(SecurityUseCase.validate)
@@ -50,14 +49,14 @@ def list_mobility_groups(
                 HTTP_404_NOT_FOUND
             )
 
-        mobility_groups_found = MobilityGroupInterface.find_all_by_conf(
+        mg_found = MobilityGroupInterface.find_all_by_conf(
             configuration
         )
 
         return UJSONResponse(
             MobilityGroupsMessages.found,
             HTTP_200_OK,
-            BsonObject.dict(mobility_groups_found)
+            BsonObject.dict(mg_found)
         )
 
     except Exception as error:
@@ -65,61 +64,6 @@ def list_mobility_groups(
             str(error),
             HTTP_400_BAD_REQUEST
         )
-
-
-@mobility_group_routes.post("/mobility_groups")
-def create_mobility_groups(
-    conf_uuid: UUID,
-    mobility_groups: List[NewMobilityGroup],
-    user = Depends(SecurityUseCase.validate)
-):
-    """
-    Create many mobility groups to specific configuration.
-
-    \f
-    :param conf_uuid: Configuration identifier
-    :param mobility_groups: Mobility groups list to insert in db
-    :param user: User authenticated.
-    """
-    try:
-        configuration = ConfigurationInterface.find_one_by_id(
-            conf_uuid,
-            user
-        )
-
-        if not configuration:
-            return UJSONResponse(
-                ConfigurationMessage.not_found,
-                HTTP_404_NOT_FOUND
-            )
-
-        if not mobility_groups:
-            return UJSONResponse(
-                MobilityGroupsMessages.not_distribution_entered,
-                HTTP_400_BAD_REQUEST
-            )
-
-        resp = []
-        for mobility_group in mobility_groups:
-            new_mobility_group = MobilityGroup(
-                **mobility_group.dict(),
-                identifier=uuid1(),
-                configuration=configuration,
-            )
-            new_mobility_group.save().reload()
-            resp.append(new_mobility_group)
-
-    except Exception as error:
-        return UJSONResponse(
-            str(error),
-            HTTP_400_BAD_REQUEST
-        )
-
-    return UJSONResponse(
-        MobilityGroupsMessages.created,
-        HTTP_201_CREATED,
-        BsonObject.dict(resp)
-    )
 
 
 @mobility_group_routes.post("/mobility_group")
@@ -132,8 +76,8 @@ def create_mobility_group(
     Create a mobility group to specific configuration.
 
     \f
-    :param conf_uuid: Configuration identifier
-    :param mobility_group: Mobility group to insert in db
+    :param conf_uuid: Configuration identifier.
+    :param mobility_group: Mobility group to insert in db.
     :param user: User authenticated.
     """
     try:
@@ -148,12 +92,6 @@ def create_mobility_group(
                 HTTP_404_NOT_FOUND
             )
 
-        if not mobility_group:
-            return UJSONResponse(
-                MobilityGroupsMessages.not_distribution_entered,
-                HTTP_400_BAD_REQUEST
-            )
-
         new_mobility_group = MobilityGroup(
             **mobility_group.dict(),
             identifier=uuid1(),
@@ -161,17 +99,62 @@ def create_mobility_group(
         )
         new_mobility_group.save().reload()
 
+        return UJSONResponse(
+            MobilityGroupsMessages.created,
+            HTTP_201_CREATED,
+            BsonObject.dict(new_mobility_group)
+        )
+
     except Exception as error:
         return UJSONResponse(
             str(error),
             HTTP_400_BAD_REQUEST
         )
 
-    return UJSONResponse(
-        MobilityGroupsMessages.created,
-        HTTP_201_CREATED,
-        BsonObject.dict(new_mobility_group)
-    )
+
+@mobility_group_routes.get("/mobility_group/{uuid}")
+def find_mobility_group(
+    conf_uuid: UUID,
+    uuid: UUID,
+    user = Depends(SecurityUseCase.validate)
+):
+    """
+    Find mobility group from specific configuration.
+
+    \f
+    :param conf_uuid: Configuration identifier
+    :param uuid: MobilityGroup identifier
+    :param user: User authenticated.
+    """
+    try:
+        configuration = ConfigurationInterface.find_one_by_id(
+            conf_uuid,
+            user
+        )
+
+        if not configuration:
+            return UJSONResponse(
+                ConfigurationMessage.not_found,
+                HTTP_404_NOT_FOUND
+            )
+
+        mg_found = MobilityGroupInterface.find_one_by_id(uuid)
+        if not mg_found:
+            return UJSONResponse(
+                MobilityGroupsMessages.not_found,
+                HTTP_404_NOT_FOUND
+            )
+        return UJSONResponse(
+            MobilityGroupsMessages.found,
+            HTTP_404_NOT_FOUND,
+            BsonObject.dict(mg_found)
+        )
+
+    except Exception as error:
+        return UJSONResponse(
+            str(error),
+            HTTP_400_BAD_REQUEST
+        )
 
 
 @mobility_group_routes.put("/mobility_group/{uuid}")
@@ -236,8 +219,8 @@ def delete_mobility_group(
     Delete a mobility group to specific configuration.
 
     \f
-    :param conf_uuid: Configuration identifier
-    :param uuid: MobilityGroup identifier
+    :param conf_uuid: Configuration identifier.
+    :param uuid: MobilityGroup identifier.
     :param user: User authenticated.
     """
     try:
@@ -272,3 +255,51 @@ def delete_mobility_group(
         MobilityGroupsMessages.deleted,
         HTTP_200_OK
     )
+
+
+@mobility_group_routes.put("/mobility_group/{uuid}/file")
+def update_distribution_file(
+    conf_uuid: UUID,
+    uuid: UUID,
+    file: UploadFile = File(...),
+    user = Depends(SecurityUseCase.validate)
+):
+    """
+
+    :param conf_uuid: Configuration identifier.
+    :param uuid: MobilityGroup identifier.
+    :param user: User authenticated.
+    :param file: File associated to distribution mobility group.
+    """
+    try:
+        configuration = ConfigurationInterface.find_one_by_id(
+            conf_uuid,
+            user
+        )
+
+        if not configuration:
+            return UJSONResponse(
+                ConfigurationMessage.not_found,
+                HTTP_404_NOT_FOUND
+            )
+
+        mg_found = MobilityGroupInterface.find_one_by_id(uuid)
+
+        if not mg_found:
+            return UJSONResponse(
+                MobilityGroupsMessages.not_found,
+                HTTP_404_NOT_FOUND
+            )
+
+        # TODO: Save and update file identifier.
+
+        return UJSONResponse(
+            MobilityGroupsMessages.file_updated,
+            HTTP_200_OK
+        )
+
+    except Exception as error:
+        return UJSONResponse(
+            str(error),
+            HTTP_400_BAD_REQUEST
+        )
