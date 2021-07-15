03b00b5e7c75c47b355138556eefdacba27b506c
David Peña <dpenah@intergrupo.com>
2021-07-15 14:51:47 -0500

    Feature: Update Natural History Routes

--- src/routes/natural_history.py
+++ src/routes/natural_history.py
@@ -3,7 +3,6 @@ from uuid import UUID, uuid1
 from fastapi import APIRouter, Depends
 from starlette.status import (
     HTTP_200_OK,
-    HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND
 )
@@ -15,7 +14,7 @@ from src.interfaces import (
     VulnerabilityGroupInterface
 )
 from src.models.db import NaturalHistory
-from src.models.route_models import NewNaturalHistory, UpdateNaturalHistory
+from src.models.route_models import UpdateNaturalHistory
 from src.use_case import SecurityUseCase
 from src.utils import BsonObject, UJSONResponse
 from src.utils import NaturalHistoryMessage
@@ -25,22 +24,25 @@ from src.utils.messages import (
     VulnerabilityGroupMessage
 )
 
-natural_history_routes = APIRouter(tags=["Natural History"])
+natural_history_routes = APIRouter(
+    prefix="/configuration/{conf_uuid}",
+    tags=["Natural History"]
+)
 
 
-@natural_history_routes.post("/configuration/{conf_uuid}/natural_history")
-def create_natural_history(
+@natural_history_routes.put("/natural_history")
+def update_natural_history(
     conf_uuid: UUID,
-    natural_history: NewNaturalHistory,
+    natural_history: UpdateNaturalHistory,
     user = Depends(SecurityUseCase.validate)
 ):
     """
-    create a new natural history
+    Create or update natural history.
 
     \f
-    :param conf_uuid:
-    :param natural_history: natural history information
-    :param user:
+    :param conf_uuid: Configuration identifier.
+    :param natural_history: Natural history to create in db.
+    :param user: User authenticated.
     """
     try:
         config_found = ConfigurationInterface.find_one_by_id(
@@ -70,41 +72,50 @@ def create_natural_history(
                 HTTP_404_NOT_FOUND
             )
 
-        history = NaturalHistory(
-            identifier=uuid1(),
-            configuration=config_found,
-            vulnerability_group=vg_found,
-            disease_group=dg_found,
-            **natural_history.dict(
-                exclude={'vulnerability_group', 'disease_group'}
+        nh_found = NaturalHistoryInterface.find_one(vg_found, dg_found)
+
+        exclude_fields = {'vulnerability_group', 'disease_group'}
+        if not nh_found:
+            nh_found = NaturalHistory(
+                **natural_history.dict(
+                    exclude=exclude_fields
+                ),
+                configuration=config_found,
+                vulnerability_group=vg_found,
+                disease_group=dg_found,
+                identifier=uuid1()
             )
-        )
-        history.save()
+            nh_found.save()
+
+        else:
+            nh_found.update(
+                **natural_history.dict(
+                    exclude=exclude_fields
+                )
+            )
+            nh_found.reload()
 
         return UJSONResponse(
-            NaturalHistoryMessage.created,
-            HTTP_201_CREATED,
-            BsonObject.dict(history)
+            NaturalHistoryMessage.updated,
+            HTTP_200_OK,
+            BsonObject.dict(nh_found)
         )
 
     except Exception as error:
-        return UJSONResponse(
-            str(error),
-            HTTP_400_BAD_REQUEST
-        )
+        return UJSONResponse(str(error), HTTP_400_BAD_REQUEST)
 
 
-@natural_history_routes.get("/configuration/{conf_uuid}/natural_history")
+@natural_history_routes.get("/natural_history")
 def list_natural_histories(
     conf_uuid: UUID,
     user = Depends(SecurityUseCase.validate)
 ):
     """
-    Get a natural history that matches the id
+    Find all natural histories from configuration.
 
     \f
-    :param conf_uuid:
-    :param user:
+    :param conf_uuid: Configuration identifier.
+    :param user: User authenticated.
     """
 
     config_found = ConfigurationInterface.find_one_by_id(
@@ -121,70 +132,8 @@ def list_natural_histories(
         config_found
     )
 
-    if not natural_history_found:
-        return UJSONResponse(
-            NaturalHistoryMessage.not_found,
-            HTTP_400_BAD_REQUEST
-        )
-
     return UJSONResponse(
         NaturalHistoryMessage.found,
         HTTP_200_OK,
         BsonObject.dict(natural_history_found)
     )
-
-
-@natural_history_routes.put("/configuration/{conf_uuid}/natural_history/{uuid}")
-def update_natural_history(
-    conf_uuid: UUID,
-    uuid: UUID,
-    natural_history: UpdateNaturalHistory,
-    user = Depends(SecurityUseCase.validate)
-):
-    """
-    Update a natural history
-
-    \f
-    :param conf_uuid:
-    :param uuid:
-    :param natural_history: natural history information
-    :param user:
-    """
-
-    try:
-        configuration = ConfigurationInterface.find_one_by_id(
-            conf_uuid,
-            user
-        )
-        if not configuration:
-            return UJSONResponse(
-                ConfigurationMessage.not_found,
-                HTTP_404_NOT_FOUND
-            )
-
-        nh_found = NaturalHistoryInterface.find_one(
-            uuid
-        )
-
-        if not nh_found:
-            return UJSONResponse(
-                NaturalHistoryMessage.not_found,
-                HTTP_404_NOT_FOUND
-            )
-
-        nh_found.update(
-            **natural_history.dict(exclude_none=True)
-        )
-        nh_found.reload()
-
-        return UJSONResponse(
-            NaturalHistoryMessage.updated,
-            HTTP_200_OK,
-            BsonObject.dict(nh_found)
-        )
-
-    except Exception as error:
-        return UJSONResponse(
-            str(error),
-            HTTP_400_BAD_REQUEST
-        )
