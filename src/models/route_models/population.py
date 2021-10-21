900f7d7427cdef56820e49a5ed26194afe8bd492
David Peña <dpenah@intergrupo.com>
2021-10-21 11:01:42 -0500

    Feature: Update population route

--- src/models/route_models/population.py
+++ src/models/route_models/population.py
@@ -1,4 +1,4 @@
-from typing import Dict, Set
+from typing import Dict, List
 
 from pydantic import BaseModel, Field
 
@@ -7,7 +7,7 @@ from src.models.general import Groups
 
 class UpdateVariable(BaseModel):
     variable: Groups = Field(...)
-    chain: Set[Groups] = Field(...)
+    chain: List[Groups] = Field(...)
     values: Dict[str, dict] = Field(None)
 
     class Config:
