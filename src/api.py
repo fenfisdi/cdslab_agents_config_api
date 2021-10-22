a4af7c815d689ffc88f142a13d3bb45912ae9510
David Peña <dpenah@intergrupo.com>
2021-10-22 11:25:25 -0500

    Feature: Implement Report Error

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ from os import environ
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.responses import PlainTextResponse
 
 from src.config import fastApiConfig
 from src.db import MongoEngine
@@ -21,9 +22,17 @@ from src.routes import (
     units_routes,
     vulnerability_group_routes
 )
+from src.services import ErrorAPI
 
 app = FastAPI(**fastApiConfig)
 db = MongoEngine().get_connection()
+
+
+@app.exception_handler(Exception)
+def http_error_report(_, exc: Exception):
+    ErrorAPI.report_error(str(exc), code=500)
+    return PlainTextResponse("Internal Server Error", status_code=500)
+
 
 app.add_middleware(
     TrustedHostMiddleware,
